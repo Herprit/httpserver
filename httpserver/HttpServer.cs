37a482cce89d6de80e40840a90c7c76b8c4a769a
Herprit <Herpritsingh12@gmail.com>
2014-09-29 13:41:58 +0200

    TCPlistener, connection

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,18 @@ namespace httpserver
 {
     public class HttpServer
     {
-        public static readonly int DefaultPort = 8888;
+        public static readonly int DefaultPort = 8080;
+        
+
+        private TcpClient connectionSocket;
+
+        public HttpServer(TcpClient connectionSocket)
+        {
+            this.connectionSocket = connectionSocket;
+        }
+
+
+        
+
     }
 }
---
+++ httpserver/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpserver
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+             TcpListener serverSocket = new TcpListener(8080);
+             serverSocket.Start();
+       
+            //Console.WriteLine("Hello http server");
+
+            TcpClient tcpConnection = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Server STARTET");
+
+            HttpServer httpServer = new HttpServer(tcpConnection);
+
+
+        }
+    }
+}
